1e8a2acc1b11f35b74ea680b43d137f9afd2ae85
Pavel <19kpd89@mail.ru>
2022-12-15 15:29:12 +0600

    homework 5

---
+++ src/main/java/ru/levelup/homework5/impl/ListDataBase.java
@@ -0,0 +1,39 @@
+package ru.levelup.homework5.impl;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.levelup.homework5.StorageSystem;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+@Service
+@ConditionalOnProperty(name = "application.cash.enable", havingValue = "true")
+public class ListDataBase implements StorageSystem {
+    private static ArrayList<Integer> listBase;
+
+    public ListDataBase(ArrayList<Integer> listBase) {
+        listBase.add(0);
+        listBase.add(1);
+        ListDataBase.listBase = listBase;
+    }
+
+    @Override
+    public void addElement(int value) {
+        IntStream.range(listBase.size(), value)
+                .forEach(i -> listBase.add(i, (listBase.get(i - 2)) + (listBase.get(i - 1))));
+    }
+
+    @Override
+    public void getElement(int value) {
+        if (listBase.size() >= value) {
+            System.out.println("Эллемент имеется в базе");
+        } else {
+            System.out.println("Эллемент добавлен в базу");
+            addElement(value);
+        }
+        System.out.println(value + "-й элемент последовательности =" + listBase.get(value - 1)
+                + "\n-----------------------");
+    }
+}
