59af0e2a91ce5266bb6ba26d93d334aa4b95fa29
Pavel <19kpd89@mail.ru>
2022-12-20 22:28:05 +0600

    correct annotation lombok

--- src/main/java/ru/levelup/homework6/model/City.java
+++ src/main/java/ru/levelup/homework6/model/City.java
@@ -1,15 +1,26 @@
 package ru.levelup.homework6.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class City {
     private Integer id;
     private String ruCityName;
     private String engCityName;
+
     private Integer population;
+
     private Integer regionId;
     //private Region region;
+
+    public City(Integer id, String ruCityName, String engCityName, Integer population, Integer regionId) {
+        this.id = id;
+        this.ruCityName = ruCityName;
+        this.engCityName = engCityName;
+        this.population = population;
+        this.regionId = regionId;
+    }
+
 }
