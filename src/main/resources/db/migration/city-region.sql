34e4bf32f9ed886ffc9f9fae2914bc5c9e10aefc
Pavel <19kpd89@mail.ru>
2022-12-21 20:21:32 +0600

    create liquibase

---
+++ src/main/resources/db/migration/city-region.sql
@@ -0,0 +1,7 @@
+insert into region values (24, 'Красноярский край');
+insert into region values (55, 'Омская обл.');
+insert into region values (54, 'Новосибирская обл.');
+
+insert into city values (3812, 'Омск', 'Omsk', 1250000, 55);
+insert into city values (391, 'Красноярск', 'Krasnoyarsk', 1187771, 24);
+insert into city values (383, 'Новосибирск', 'Novosibirsk', 1633595, 54);
