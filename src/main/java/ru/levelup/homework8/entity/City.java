8df81c92c8c861002cd6cf29f64c4fc7486f3ca8
Pavel <19kpd89@mail.ru>
2023-01-02 18:57:35 +0600

    validation

--- src/main/java/ru/levelup/homework8/entity/City.java
+++ src/main/java/ru/levelup/homework8/entity/City.java
@@ -2,8 +2,11 @@ package ru.levelup.homework8.entity;
 
 
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "city")
@@ -12,18 +15,22 @@ import javax.persistence.*;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Validated
 public class City {
 
     @Id
+    @Min(0)
     private Integer id;
     @Column(name = "ru_city_name")
     private String ruCityName;
     @Column(name = "eng_city_name")
     private String enCityName;
     @Column(name = "population")
+    @Min(0)
     private Integer population;
     @ManyToOne
     @JoinColumn(name = "region_id")
+    @NotBlank
     private Region region;
 
 }
--- src/main/java/ru/levelup/homework8/entity/Region.java
+++ src/main/java/ru/levelup/homework8/entity/Region.java
@@ -1,11 +1,14 @@
 package ru.levelup.homework8.entity;
 
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "region")
@@ -14,10 +17,13 @@ import javax.persistence.Table;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Validated
 public class Region {
     @Id
+    @Min(0)
     private Integer id;
 
     @Column(name ="name")
+    @NotBlank
     private String name;
 }
